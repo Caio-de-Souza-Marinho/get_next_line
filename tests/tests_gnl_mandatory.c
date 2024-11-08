d5c120881526ad0ce1d83d868a17111883108e86
Caio De Souza Marinho <caide-so@c1r3p4.42sp.org.br>
2024-11-07 21:28:54 -0300

    tests: add first test and Makefile

--- tests/tests_gnl_mandatory.c
+++ tests/tests_gnl_mandatory.c
@@ -8,11 +8,28 @@ MU_TEST(gnl_mandatory_test)
 {
 	// ARRANGE
 	int	fd1 = open("test_files/test1.txt", O_RDONLY);
+	mu_check(fd1 != -1); // Assert that the file opened successfully
+
+	const char	*expected_lines[] = 	
+		{
+			"Hello_Dolly.\n",
+			"Goodbye.\n",
+			"Hello_Dolly.\n",
+			NULL,
+		};
 
 	// ACT
-	char	*actual_result = get_next_line(fd1);
-	
-	// ASSERT
+	for (int i = 0; expected_lines[i] != NULL; i++)
+	{
+		char	*actual_result = get_next_line(fd1);
+
+		// ASSERT
+		mu_assert_string_eq(expected_lines[i], actual_result);
+
+		// FREE MEMORY
+		free(actual_result);
+	}	
+	close(fd1);
 }
 
 MU_TEST_SUITE(get_next_line_test_suite)
